9c3597996e9d17d5461b099c6e939f3d1d3f3cef
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-04-25 13:25:19 +0530

    cli: add commands: about,version,help

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+//csm toolchain version
+const VERSION: &str = "0.1.0";
 fn main() {
-    println!("Hello, world!");
+    let args:Vec<String> = std::env::args().collect();
+
+    for arg in &args{
+        if args.len() == 1 {
+            println!("csm {VERSION}");
+            println!("toolchain for csm programming language.");
+            println!("for more info:\n csm --help");
+        }else if args.len() == 2 {
+            if arg == "--version" || arg == "-v" {
+                println!("csm {VERSION}");
+            }else if arg == "--help" || arg == "-h" {
+                println!("csm commands:");
+                println!(" 1. csm <no args>                 -> prints about message.");
+                println!(" 2. csm --version                 -> prints csm version.");
+                println!(" 3. csm --help                    -> prints this commands list.");
+                println!(" 4. csm main.csm                  -> compiles to single exe binary.");
+                println!(" 5. csm main.csm -o <output-name> -> compiles to single exe binary with output name.");        
+                println!(" 6. csm init                      -> initializes a new project in current directory.");
+                println!(" 7. csm run                       -> run current directory(project).");
+            }
+        }
+    }
 }
